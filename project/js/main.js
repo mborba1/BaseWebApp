34c8fa3732eee795d239705166ffd9219f90846a
Margareth Borba <31484293+mborba1@users.noreply.github.com>
2020-05-28 11:36:09 -0400

    updates for myapp

--- project/js/main.js
+++ project/js/main.js
@@ -1,4 +1,99 @@
-function showPicture(){
+$(document).ready(function(){
+  getPosts();
+})
+
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function (result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+function addMessage(postTitle, postBody){
+  var postData = {
+    title: postTitle,
+    body: postBody
+  }
+  var database = firebase.database().ref("posts");
+
+  var newPostRef = database.push();
+  newPostRef.set(postData, function (error) {
+    if (error) {
+      console.log("An error has ocurred!")
+    } else {
+      // Data saved successfully!
+      window.location.reload();
+    }
+  });
+}
+
+function handleMessageFormSubmit(){
+  var postTitle = $("#post-title").val();
+  var postBody = $("#post-body").val();
+
+  addMessage(postTitle, postBody)
+}
+
+function getPosts(){
+  return firebase.database().ref('posts').once('value').then(function (snapshot) {
+    var posts = snapshot.val();
+    console.log(posts);
+    
+    for(var postKey in posts){
+      var post = posts[postKey];
+      $("#post-listing").append("<div>"+post.title+" - "+post.body+"</div>");
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+//======== Weather session ================
+/*$(document).ready(function(){
+  getWeather();
+})
+
+function getWeather(searchQuery){
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid="+apiKey;
+  
+  
+
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+
+  }})
+}
+
+function searchWeather(){
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
+}*/
+ 
+
+//========== Show Picture session ===================
+/*function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
@@ -6,4 +101,4 @@ function showPicture(){
 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
-}
+}*/
